993d3f770306d90c1bdd26f80b2d5b3ab7a47e38
Jorge Capona <jcapona@gmail.com>
2016-01-11 13:28:23 -0300

    Displays view/index.jade

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+var net = require('net');
+
+var port = 5000;
+
+var server = net.createServer(function(socket){
+    var date = new Date();
+    var month = Number(date.getMonth())+1 < 10? "0"+(Number(date.getMonth())+1).toString() : (Number(date.getMonth())+1).toString();
+    var day = Number(date.getDate()) < 10? "0"+date.getDate() : date.getDate();
+    socket.end(date.getFullYear()+"-"+month+"-"+day+" "+date.getHours()+":"+date.getMinutes());
+    console.log(socket);
+});
+
+server.listen(port);
+*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'jade');
+
+app.get('/', function(request, response) {
+  response.render("index");
+});
+
+app.listen(app.get('port'));
+
+

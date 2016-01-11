4e6aeaee3ab02b6d1de085d6d17736a3cfbc82c2
Jorge Capona <jcapona@gmail.com>
2016-01-11 16:29:16 -0300

    Partially working version. Doesnt check correctly natural language dates

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-/*
-var net = require('net');
-
-var port = 5000;
-
-var server = net.createServer(function(socket){
-    var date = new Date();
-    var month = Number(date.getMonth())+1 < 10? "0"+(Number(date.getMonth())+1).toString() : (Number(date.getMonth())+1).toString();
-    var day = Number(date.getDate()) < 10? "0"+date.getDate() : date.getDate();
-    socket.end(date.getFullYear()+"-"+month+"-"+day+" "+date.getHours()+":"+date.getMinutes());
-    console.log(socket);
-});
-
-server.listen(port);
-*/
-
 var express = require('express');
 var app = express();
 
@@ -26,10 +10,42 @@ app.get("/", function(request, response) {
 
 app.get("/:date", function(request, response) {
   var date = request.params.date;
-  console.log(date);
-  response.render("index",{"quote":date});
+  var unixDate = null;
+  var naturalDate = null;
+
+  if(date.match(/^\d+$/g) !== null) // unix
+  {
+    unixDate = Number(date);
+    naturalDate = toNatural(date);
+  }
+  else
+  {
+    var month = date.split(",")[0].split(" ")[0];
+    var day = date.split(",")[0].split(" ")[1];
+    var year = date.split(",")[1];
+    if((month!==undefined)&&(day!==undefined)&&(year!==undefined))
+    {  
+      naturalDate = date;
+      unixDate = toUnix(year,month,day);
+    }
+  }
+  var json = JSON.stringify({"unix":unixDate,"natural":naturalDate});
+  response.send(json);
+
 });
 
 app.listen(app.get('port'));
 
+function toNatural(dateParam)
+{
+  var date = new Date();
+  date.setTime(Number(dateParam)*1000);
+  var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  return (months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear());
+}
 
+function toUnix(year,month,day)
+{
+  var date = new Date(month+" "+day+", "+year);
+  return Number(date.getTime()/1000);
+}
